58d72caee222a4452bf22aacff6acca578598cfe
Yashodhar <mahajan.yash09@gmail.com>
2024-02-19 22:18:08 +0100

    some update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pytz
 import requests
 from random import choice
 import os
+import urllib3
 
 # Bot Token
 TOKEN = '7078028281:AAGPuTi-8fiHrkvacxD-EfmpbG6b0YJebYs'
@@ -19,16 +20,30 @@ logger = logging.getLogger(__name__)
 GITHUB_REPO_URL = 'https://github.com/4rchB1sh0p/german_flashcards_telegram_bot/blob/main/flashcards/'
 GITHUB_RAW_URL = 'https://raw.githubusercontent.com/4rchB1sh0p/german_flashcards_telegram_bot/main/flashcards/'
 
+def generate_flashcard_names():
+    """
+    Generate a list of flashcard filenames based on the naming convention.
+    The list will include both .jpg and .png files from name_001 to name_300.
+    """
+    flashcard_names = []
+    for i in range(1, 301):  # Generates numbers from 1 to 300
+        number_str = f'{i:03}'  # Formats the number as a three-digit string (001, 002, ..., 300)
+        # Add both .jpg and .png filenames for each number
+        flashcard_names.append(f'name_{number_str}.jpg')
+        flashcard_names.append(f'name_{number_str}.png')
+    return flashcard_names
+
+# Now use this function to fetch a random flashcard
 def fetch_random_flashcard():
     """
-    Fetch a random flashcard from the GitHub repository.
+    Fetch a random flashcard URL from dynamically generated names,
+    supporting both .jpg and .png files.
     """
-    # Simulate fetching file names. Ideally, you should have a way to list these files via GitHub API or a static list.
-    # Here, we're assuming the presence of a static list of filenames for simplicity.
-    flashcard_names = ['name1.jpg', 'name2.jpg', 'name3.jpg']  # Replace with actual file names
-    selected_flashcard = choice(flashcard_names)
-    flashcard_url = GITHUB_RAW_URL + selected_flashcard
+    flashcard_names = generate_flashcard_names()  # Generate the list of flashcard names
+    selected_flashcard = choice(flashcard_names)  # Randomly select a flashcard
+    flashcard_url = f'https://raw.githubusercontent.com/4rchB1sh0p/german_flashcards_telegram_bot/main/flashcards/{selected_flashcard}'
     return flashcard_url
+
 
 def send_flashcard(update: Update, context: CallbackContext):
     """
@@ -68,7 +83,8 @@ def main():
     """
     Main function to start the bot.
     """
-    updater = Updater(token=TOKEN, use_context=True)
+    #updater = Updater(TOKEN)
+    updater = Updater(token=TOKEN,use_context=True)
     dp = updater.dispatcher
 
     # Command Handlers
